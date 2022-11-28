119e1a098d62280360945a07a61821c900c3c3d4
Felipe Lira <filipi6219@gmail.com>
2022-11-28 20:35:10 -0300

    add arquivos para ilustrar uso de módulos Ecma em JS

---
+++ capitulo-02/modules/calculator.js
@@ -0,0 +1,7 @@
+import { circleArea, squareArea } from "./calcArea.js";
+
+const r = 4;
+const s = 70;
+
+console.log(`para r = ${r}, a área do círculo é: ${circleArea(4)}`);
+console.log(`para s = ${s}, a área do quadrado é: ${squareArea(s)}`);
